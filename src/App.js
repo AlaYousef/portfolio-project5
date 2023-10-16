5a11cc18773cf54a4720a512a2574453f16ad815
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-16 11:18:41 +0000

    setup Reat boostrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcom to my pp5</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
