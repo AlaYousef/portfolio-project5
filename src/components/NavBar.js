b7587324760058e8b72331a1769b3c7cf2f6278c
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-17 10:07:00 +0000

    styles navbar/ app components, adding fontawesome icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Navbar, Container, Nav, Form } from "react-bootstrap";
 import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css'
 
 function NavBar() {
   return (
-    <Navbar bg="light" variant="light" fixed="top" expand="md">
+    <Navbar className={styles.NavBar}  fixed="top" expand="md">
     <Container>
       <Navbar.Brand><img src={logo} alt='logo' height='60' /></Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="ml-auto">
-            <Nav.Link >Home</Nav.Link>
-            <Nav.Link >Sign in</Nav.Link>
-            <Nav.Link >Sign up</Nav.Link>
+          <Nav className="ml-auto text-left">
+            <Nav.Link ><i className="fa-solid fa-house"></i> Home</Nav.Link>
+            <Nav.Link ><i className="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
+            <Nav.Link ><i className="fas fa-user-plus"></i> Sign up</Nav.Link>
           </Nav>
       </Navbar.Collapse>
     </Container>
