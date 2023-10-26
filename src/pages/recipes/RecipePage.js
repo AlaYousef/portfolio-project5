e1eab256c12413eb003a13d8ae7181fac3c30944
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-26 09:21:30 +0000

    Adding postPage component

--- src/pages/recipes/RecipePage.js
+++ src/pages/recipes/RecipePage.js
@@ -1,17 +1,37 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
 function RecipePage() {
+
+    const { id } = useParams();
+    const [recipe, setRecipe] = useState({ results: [] });
+  
+    useEffect(() => {
+      const handleMount = async () => {
+        try {
+          const [{ data: recipe }] = await Promise.all([
+            axiosReq.get(`/recipes/${id}`),
+          ]);
+          setRecipe({ results: [recipe] });
+          console.log(recipe);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+  
+      handleMount();
+    }, [id]);
 
     return (
         <Row className="h-100">
           <Col className="py-2 p-0 p-lg-2" lg={8}>
             <p>Popular profiles for mobile</p>
-            <p>Post component</p>
+            <p>Recipe component</p>
             <Container className={appStyles.Content}>
               Comments
             </Container>
