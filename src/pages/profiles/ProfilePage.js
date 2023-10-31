99140edbb17cf2e2662ef562ae9056431408ce4f
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-10-31 14:31:34 +0000

    Adding follow functionality

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -26,7 +26,7 @@ function ProfilePage() {
   const [profileRecipes, setProfileRecipes] = useState({ results: [] });
   const currentUser = useCurrentUser();
   const { id } = useParams();
-  const setProfileData = useSetProfileData();
+  const { setProfileData, handleFollow } = useSetProfileData();
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
@@ -91,7 +91,7 @@ function ProfilePage() {
             ) : (
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Black}`}
-                onClick={() => {}}
+                onClick={() => handleFollow(profile)}
               >
                 follow
               </Button>
